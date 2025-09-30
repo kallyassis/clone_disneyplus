f0c50f1c855ddd5e842379b7666a950df33829ba
kalinka <kallysantosk@gmail.com>
2025-09-30 15:16:09 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const imagemin = require('gulp-imagemin');
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/styles/.css'));
+    .pipe(gulp.dest('./dist/styles/'));
 }
 
 function images() {
